1bb6327dfd07d7b1920097a0a1818d9c98ce3ceb
acute <awaisraobuddy@gmail.com>
2024-06-20 12:47:16 +0500

    Header done

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// ===== Show Navbar =====
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    const Navbar =  document.getElementById('navbar-top');
+
+    window.addEventListener('scroll', function () {
+
+        window.scrollY > 400 ? Navbar.classList.add('fixed-top', 'shadow') : Navbar.classList.remove('fixed-top', 'shadow');
+
+    });
+});
