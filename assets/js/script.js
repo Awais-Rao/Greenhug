03028bcd0618a6b62c9d0da0edd6c48bc6ef3cad
acute <awaisraobuddy@gmail.com>
2024-06-23 17:39:29 +0500

    Preloader added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 // ===== Show Navbar =====
 
 document.addEventListener("DOMContentLoaded", function () {
-
+    
     const Navbar =  document.getElementById('navbar-top');
 
     window.addEventListener('scroll', function () {
@@ -12,4 +12,19 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
+// ===== Preloader =====
 
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Add an event listener to run once the page is fully loaded
+    window.addEventListener('load', function() {
+        // Fade out the preloader
+        let preloader = document.getElementById('preloader');
+        preloader.style.opacity = '0';
+
+        // Once the preloader is fully faded out, remove it from the DOM
+        setTimeout(function() {
+            preloader.style.display = 'none';
+        }, 500); // Match this duration to the transition duration
+    });
+});
