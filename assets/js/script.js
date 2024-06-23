d9ab493d343ff98309620d73ba508ede17b21583
acute <awaisraobuddy@gmail.com>
2024-06-24 01:02:02 +0500

    Animation Added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,62 @@
 // ===== Show Navbar =====
 
 document.addEventListener("DOMContentLoaded", function () {
-    
-    const Navbar =  document.getElementById('navbar-top');
+  const Navbar = document.getElementById("navbar-top");
 
-    window.addEventListener('scroll', function () {
-
-        window.scrollY > 300 ? Navbar.classList.add('fixed-top', 'shadow') : Navbar.classList.remove('fixed-top', 'shadow');
-
-    });
+  window.addEventListener("scroll", function () {
+    window.scrollY > 300
+      ? Navbar.classList.add("fixed-top", "shadow")
+      : Navbar.classList.remove("fixed-top", "shadow");
+  });
 });
 
+// ========= Preloader ========
 
-// ===== Preloader =====
+document.addEventListener("DOMContentLoaded", function () {
+  window.addEventListener("load", function () {
+    let preloader = document.getElementById("preloader");
+    preloader.style.opacity = "0";
 
-
-document.addEventListener("DOMContentLoaded", function() {
-    // Add an event listener to run once the page is fully loaded
-    window.addEventListener('load', function() {
-        // Fade out the preloader
-        let preloader = document.getElementById('preloader');
-        preloader.style.opacity = '0';
-
-        // Once the preloader is fully faded out, remove it from the DOM
-        setTimeout(function() {
-            preloader.style.display = 'none';
-        }, 500); // Match this duration to the transition duration
-    });
+    // Once the preloader is fully faded out, remove it from the DOM
+    setTimeout(function () {
+      preloader.style.display = "none";
+    }, 500);
+  });
 });
+
+// ========= Scroll Reveal ========
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 3000,
+  delay: 400,
+  // reset: true
+});
+
+// sr.reveal(`.home__data, .explore__data, .explore__user`);
+// sr.reveal(`.home__card`, { delay: 600, distance: "100px", interval: 200 });
+// sr.reveal(`.about__data, .join__image`, { origin: "right" });
+// sr.reveal(`.about__image, .join__data`, { origin: "left" });
+// sr.reveal(`.popular__card`, { interval: 200 });
+
+
+// --------- Home ---------
+
+sr.reveal(`.goal__card, .years__swiper, .experience__hero_img, .experience__number_container, .experience__card, .talk__environment_image, .lifestyle__image_wrapper, .upscaling__hero, .individual__hero_data, .individual__company_ranking`)
+sr.reveal(`.number__card_small-wrapper, .history__hero_text, .monitoring__data, .lifestyle__header_text, .lifestyle__button_wrapper, .ranking__header_left, .clarity__item, .individual__impact_card`, { origin: "right" });
+sr.reveal(`.header__data, .number__card_big-wrapper, .history__hero_heading, .experience__hero_title,.talk__hero_data, .talk__podcast_text,  .lifestyle__header_heading, .individual__impact_card-big `, { origin: "left" });
+sr.reveal(`.partner__img_wrapper, .upscaling__project_card, .simple__impact_card`, { origin: "bottom" });
+sr.reveal(`.card__wrapper, .partner__img_wrapper, .experience__gallery_image, .step__card, .upscaling__project_card, .ranking__advantage_card, .simple__impact_card`, { interval: 300, distance: "100px" });
+
+sr.reveal(`.talk__environment_point, .upscaling__result_card`, { origin: "left", interval: 300, distance: "100px" });
+
+sr.reveal(`.ranking__points_bar`, { origin: "left", interval: 200 });
+
+
+
+// --------- Footer ---------
+
+// sr.reveal(`.footer__upper, .footer__line, .footer__bottom`);
