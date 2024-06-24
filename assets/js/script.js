e5e34b6158cb76adc70bb34089d6e484d64dcbf3
acute <awaisraobuddy@gmail.com>
2024-06-24 12:21:59 +0500

    Finalization

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,7 +24,6 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-// ========= Scroll Reveal ========
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 
@@ -36,24 +35,27 @@ const sr = ScrollReveal({
   // reset: true
 });
 
-// sr.reveal(`.home__data, .explore__data, .explore__user`);
-// sr.reveal(`.home__card`, { delay: 600, distance: "100px", interval: 200 });
-// sr.reveal(`.about__data, .join__image`, { origin: "right" });
-// sr.reveal(`.about__image, .join__data`, { origin: "left" });
-// sr.reveal(`.popular__card`, { interval: 200 });
 
 
-// --------- Home ---------
-
-sr.reveal(`.goal__card, .years__swiper, .experience__hero_img, .experience__number_container, .experience__card, .talk__environment_image, .lifestyle__image_wrapper, .upscaling__hero, .individual__hero_data, .individual__company_ranking`)
+sr.reveal(`.years__swiper, .experience__hero_img, .experience__number_container, .experience__card, .talk__environment_image, .lifestyle__image_wrapper, .upscaling__hero, .individual__hero_data, .individual__company_ranking`)
 sr.reveal(`.number__card_small-wrapper, .history__hero_text, .monitoring__data, .lifestyle__header_text, .lifestyle__button_wrapper, .ranking__header_left, .clarity__item, .individual__impact_card`, { origin: "right" });
 sr.reveal(`.header__data, .number__card_big-wrapper, .history__hero_heading, .experience__hero_title,.talk__hero_data, .talk__podcast_text,  .lifestyle__header_heading, .individual__impact_card-big `, { origin: "left" });
 sr.reveal(`.partner__img_wrapper, .upscaling__project_card, .simple__impact_card`, { origin: "bottom" });
-sr.reveal(`.card__wrapper, .partner__img_wrapper, .experience__gallery_image, .step__card, .upscaling__project_card, .ranking__advantage_card, .simple__impact_card`, { interval: 300, distance: "100px" });
+sr.reveal(`.partner__img_wrapper, .experience__gallery_image, .step__card, .upscaling__project_card, .ranking__advantage_card, .simple__impact_card`, { interval: 300, distance: "100px" });
 
 sr.reveal(`.talk__environment_point, .upscaling__result_card`, { origin: "left", interval: 300, distance: "100px" });
 
 sr.reveal(`.ranking__points_bar`, { origin: "left", interval: 200 });
+
+
+
+  // Check the screen width and add animation on large screens only
+  if (window.innerWidth >= 576) {
+
+    sr.reveal(`.goal__card`);
+    sr.reveal(`.card__wrapper`, { interval: 300, distance: "100px" });
+
+}
 
 
 
